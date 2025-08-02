5ee7de48164414f435c2b130a802cb29f3c9b1af
Amrit Kumar Singh <113255930+AmritSingh1502@users.noreply.github.com>
2025-08-03 01:03:11 +0530

    added a winston logger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const {  PORT } = require('./config');
+const {  ServerConfig , Logger } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
 
 app.use('/api', apiRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Server is running on port ${ServerConfig.PORT};`);
 }); 
 
